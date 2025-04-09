b70078468aa16d1de2364e5645e05f37ad0d4cbd
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-09 09:40:28 +0530

    changes in privacy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,10 +1,12 @@
 
 export default async function Page() {
     return (
-        <main className="container mx-auto ">
-        <h1 className="text-6xl font-bold">
-            Privacy Policy
-        </h1>
+        <main className="py-20 px-3">
+            <div className="container mx-auto">
+                <h1 className="text-6xl font-bold">
+                    Privacy Policy
+                </h1>
+            </div>
         </main>
     )
 }
