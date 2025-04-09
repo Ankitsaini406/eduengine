b23aa808c2cc22f2dc415ae5562e3751838debb2
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-09 09:38:21 +0530

    changes page and add page

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+
+export default async function Page() {
+    return (
+        <main className="container mx-auto ">
+        <h1 className="text-6xl font-bold">
+            Privacy Policy
+        </h1>
+        </main>
+    )
+}
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,10 +15,10 @@ export default function Footer() {
 
                 {/* Navigation Links */}
                 <nav className="flex space-x-6 mt-4 md:mt-0">
-                    <Link href="/privacy" className="text-sm hover:text-white transition">
+                    <Link href="/privacy-policy" className="text-sm hover:text-white transition">
                         Privacy Policy
                     </Link>
-                    <Link href="/terms" className="text-sm hover:text-white transition">
+                    <Link href="/terms-service" className="text-sm hover:text-white transition">
                         Terms of Service
                     </Link>
                     <Link href="/contact" className="text-sm hover:text-white transition">
