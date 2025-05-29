de255c6e686688528886ec80ca2155c60025a50b
Ankit Saini <as.ankitsaini406@gmail.com>
2025-05-29 12:49:50 +0530

    littel change in services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+        <div className="min-h-screen px-4 py-16">
+            <div className="max-w-7xl mx-auto">
+                <h2 className="text-4xl font-bold text-white mb-8">Services</h2>
+            </div>
+        </div>
+    );
+}
