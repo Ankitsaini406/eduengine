b0b67d2844984db3cc49d472a926d5f81a0c3964
Ankit Saini <as.ankitsaini406@gmail.com>
2025-06-13 17:43:50 +0530

    make changes in sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,20 +1,20 @@
 export default async function sitemap() {
     const apiPoint =
         process.env.NODE_ENV === "development"
-            ? process.env.LOCAL_URL
-            : process.env.HOST_URL;
+            ? 'http://localhost:3002/'
+            : 'https://eduengine.in/';
 
     const staticPages = [
         `${apiPoint}`,
-        `${apiPoint}/about-us`,
-        `${apiPoint}/contact-us`,
-        `${apiPoint}/careers`,
-        `${apiPoint}/services/app-development`,
-        `${apiPoint}/services/cloud-computing`,
-        `${apiPoint}/services/cyber-security`,
-        `${apiPoint}/services/machine-learning`,
-        `${apiPoint}/services/graphic-design`,
-        `${apiPoint}/services/web-development`,
+        `${apiPoint}about-us`,
+        `${apiPoint}contact-us`,
+        `${apiPoint}careers`,
+        `${apiPoint}services/app-development`,
+        `${apiPoint}services/cloud-computing`,
+        `${apiPoint}services/cyber-security`,
+        `${apiPoint}services/machine-learning`,
+        `${apiPoint}services/graphic-design`,
+        `${apiPoint}services/web-development`,
     ];
 
     return staticPages.map((url) => ({
