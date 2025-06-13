ae757e883019adabc58500e0baccd484073db925
Ankit Saini <as.ankitsaini406@gmail.com>
2025-06-13 17:48:29 +0530

    change splash

--- src/app/SplashScreen.tsx
+++ src/app/SplashScreen.tsx
@@ -31,7 +31,7 @@ const nameVariants = {
 //     },
 // };
 
-export default function SplashScreen(): JSX.Element | null {
+export default function SplashScreen(): TSX.Element | null {
     const [isVisible, setIsVisible] = useState<boolean>(false);
     const [isMounted, setIsMounted] = useState<boolean>(false);
 
